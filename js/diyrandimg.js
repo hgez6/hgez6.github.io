04e8659d08bacb71cc0fed490bbbd6d1624a7096
hgez6 <1799785832@qq.com>
2022-01-11 15:36:29 +0800

    1-11

---
+++ js/diyrandimg.js
@@ -0,0 +1,25 @@
+// //随机背景图片数组,图片可以换成图床链接，注意最后一条后面不要有逗号
+// var backimg =[
+//     "url(/img/bg1.JPG)",
+//     "url(/img/bg2.jpg)",
+//     "url(/img/bg3.jpg)",
+//     "url(/img/bg4.jpg)"
+//   ];
+//   //获取背景图片总数，生成随机数
+//   var bgindex =Math.floor(Math.random() * backimg.length);
+//   //重设背景图片
+//   document.getElementById("web_bg").style.backgroundImage = backimg[bgindex];
+  //随机banner数组,图片可以换成图床链接，注意最后一条后面不要有逗号
+  var bannerimg =[
+    "url(https://ae01.alicdn.com/kf/H21b5f6b8496141a1979a33666e1074d9x.jpg)",
+    "url(https://picsum.photos/id/767/3069/2048)",
+    "url(https://picsum.photos/id/800/4663/3109)",
+    "url(https://picsum.photos/id/459/2310/1534)",
+    "url(https://picsum.photos/id/458/5184/3456)",
+    "url(https://picsum.photos/id/317/1935/1089)"
+  ];
+  //获取banner图片总数，生成随机数
+  var bannerindex =Math.floor(Math.random() * bannerimg.length);
+  //重设banner图片
+  document.getElementById("page-header").style.backgroundImage = bannerimg[bannerindex];
+  
---
+++ js/diytitle.js
@@ -0,0 +1,18 @@
+//动态标题
+var OriginTitile = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        //离开当前页面时标签显示内容
+        document.title = 'w(ﾟДﾟ)w 大鸟准备起飞！';
+        clearTimeout(titleTime);
+    }
+    else {
+        //返回当前页面时标签显示内容
+        document.title = '♪(^∇^*)欢迎回来！' + OriginTitile;
+        //两秒后变回正常标题
+        titleTime = setTimeout(function () {
+            document.title = OriginTitile;
+        }, 2000);
+    }
+});
