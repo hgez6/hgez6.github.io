98c44618e5498af5e87f60fb3288745f34a95af5
hgez6 <1799785832@qq.com>
2022-01-12 11:44:22 +0800

    CDN

---
+++ js/diytianq.js
@@ -0,0 +1,27 @@
+
+
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "1",
+    "tmpColor": "FFFFFF",
+    "tmpSize": "16",
+    "cityColor": "FFFFFF",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "borderRadius": "16",
+    "fixed": "true",
+    "vertical": "top",
+    "horizontal": "left",
+    "left": "190",
+    "top": "10",
+    "key": "a27c8321c04744afa317bebb517d2a49"
+  }
+}
+
