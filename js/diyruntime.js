71591d878dfb264e119e2a4486b77f6b7af1d7e1
hgez6 <1799785832@qq.com>
2022-01-16 10:02:17 +0800

    加载动画

---
+++ js/diyruntime.js
@@ -0,0 +1,36 @@
+setInterval(() => {
+  let create_time = Math.round(new Date('2021/12/30 16:8:8').getTime() / 1000); //在此行修改建站时间
+  let timestamp = Math.round((new Date().getTime()) / 1000);
+  let second = timestamp - create_time;
+  let time = new Array(0, 0, 0, 0, 0);
+
+  var nol = function(h){
+    return h>9?h:'0'+h;
+  }
+  if (second >= 365 * 24 * 3600) {
+    time[0] = parseInt(second / (365 * 24 * 3600));
+    second %= 365 * 24 * 3600;
+  }
+  if (second >= 24 * 3600) {
+    time[1] = parseInt(second / (24 * 3600));
+    second %= 24 * 3600;
+  }
+  if (second >= 3600) {
+    time[2] = nol(parseInt(second / 3600));
+    second %= 3600;
+  }
+  if (second >= 60) {
+    time[3] = nol(parseInt(second / 60));
+    second %= 60;
+  }
+  if (second > 0) {
+    time[4] = nol(second);
+  }
+  if ((Number(time[2])<21) && (Number(time[2])>6)){
+    currentTimeHtml ="<img class='boardsign' src='https://img.shields.io/badge/本大鸟-飞行中-6adea8?style=social&logo=ApacheRocketMQ' title='加速提高效率准备着陆~'><div id='runtime'>" + time[0] + ' YEAR ' + time[1] + ' DAYS ' + time[2] + ' : ' + time[3] + ' : ' + time[4] + '</div>';
+  }
+  else{
+    currentTimeHtml ="<img class='boardsign' src='https://img.shields.io/badge/大鸟在-睡觉觉-6adea8?style=social&logo=coffeescript' title='这个点了早就去睡觉啦，熬夜对身体不好哦~'><div id='runtime'>" + time[0] + ' YEAR ' + time[1] + ' DAYS ' + time[2] + ' : ' + time[3] + ' : ' + time[4] + '</div>';
+  }
+  document.getElementById("workboard").innerHTML = currentTimeHtml;
+}, 1000);
