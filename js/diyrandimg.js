6dd3518b588310c20c8fee20026e7d6893bf6484
hgez6 <1799785832@qq.com>
2022-04-24 13:30:45 +0800

    搜索可用文字

--- js/diyrandimg.js
+++ js/diyrandimg.js
@@ -10,34 +10,34 @@
 //   //重设背景图片
 //   document.getElementById("web_bg").style.backgroundImage = backimg[bgindex];
   //随机banner数组,图片可以换成图床链接，注意最后一条后面不要有逗号
-  // var bannerimg =[
-  //   "url(https://z4a.net/images/2022/04/19/IMG_20220307_190940.jpg)",
-  //   "url(https://z4a.net/images/2022/04/19/IMG_20220309_185813.jpg)",
-  //   "url(https://z4a.net/images/2022/04/19/IMG_20220310_103158.jpg)",
-  //   "url(https://z4a.net/images/2022/04/19/IMG_20220226_131802.jpg)",
-  //   "url(https://z4a.net/images/2022/04/19/IMG_20220310_103231.jpg)",
-  //   "url(https://z4a.net/images/2022/04/19/IMG_20220308_142522.jpg)",
-  //   "url(https://z4a.net/images/2022/04/19/IMG_20220308_142505.jpg)"
-  // ];
-  //本站源
   var bannerimg =[
-    "url(https://e5-oneindex.vercel.app/api/raw/?path=/picbed/IMG_20220108_180309.jpg)",
-    "url(https://e5-oneindex.vercel.app/api/raw/?path=/picbed/IMG_20220112_173310.jpg)",
-    "url(https://e5-oneindex.vercel.app/api/raw/?path=/picbed/IMG_20220112_173514.jpg)",
-    "url(https://e5-oneindex.vercel.app/api/raw/?path=/picbed/IMG_20220118_134344.jpg)",
-    "url(https://e5-oneindex.vercel.app/api/raw/?path=/picbed/IMG_20220226_131802.jpg)",
-    "url(https://e5-oneindex.vercel.app/api/raw/?path=/picbed/IMG_20220308_142505.jpg)",
-    "url(https://e5-oneindex.vercel.app/api/raw/?path=/picbed/IMG_20220309_185813.jpg)",
-    "url(https://e5-oneindex.vercel.app/api/raw/?path=/picbed/IMG_20220308_142505.jpg)",
-    "url(https://e5-oneindex.vercel.app/api/raw/?path=/picbed/IMG_20220310_103203.jpg)",
-    "url(https://e5-oneindex.vercel.app/api/raw/?path=/picbed/IMG_20220310_103210.jpg)",
-    "url(https://e5-oneindex.vercel.app/api/raw/?path=/picbed/IMG_20220310_103220.jpg)",
-    "url(https://e5-oneindex.vercel.app/api/raw/?path=/picbed/IMG_20220310_103231.jpg)",
-    "url(https://e5-oneindex.vercel.app/api/raw/?path=/picbed/IMG_20220307_190940.jpg)"
-    
-    
-    
+    "url(https://z4a.net/images/2022/04/19/IMG_20220307_190940.jpg)",
+    "url(https://z4a.net/images/2022/04/19/IMG_20220309_185813.jpg)",
+    "url(https://z4a.net/images/2022/04/19/IMG_20220310_103158.jpg)",
+    "url(https://z4a.net/images/2022/04/19/IMG_20220226_131802.jpg)",
+    "url(https://z4a.net/images/2022/04/19/IMG_20220310_103231.jpg)",
+    "url(https://z4a.net/images/2022/04/19/IMG_20220308_142522.jpg)",
+    "url(https://z4a.net/images/2022/04/19/IMG_20220308_142505.jpg)"
   ];
+  //本站源
+  // var bannerimg =[
+  //   "url(https://e5-oneindex.vercel.app/api/raw/?path=/picbed/IMG_20220108_180309.jpg)",
+  //   "url(https://e5-oneindex.vercel.app/api/raw/?path=/picbed/IMG_20220112_173310.jpg)",
+  //   "url(https://e5-oneindex.vercel.app/api/raw/?path=/picbed/IMG_20220112_173514.jpg)",
+  //   "url(https://e5-oneindex.vercel.app/api/raw/?path=/picbed/IMG_20220118_134344.jpg)",
+  //   "url(https://e5-oneindex.vercel.app/api/raw/?path=/picbed/IMG_20220226_131802.jpg)",
+  //   "url(https://e5-oneindex.vercel.app/api/raw/?path=/picbed/IMG_20220308_142505.jpg)",
+  //   "url(https://e5-oneindex.vercel.app/api/raw/?path=/picbed/IMG_20220309_185813.jpg)",
+  //   "url(https://e5-oneindex.vercel.app/api/raw/?path=/picbed/IMG_20220308_142505.jpg)",
+  //   "url(https://e5-oneindex.vercel.app/api/raw/?path=/picbed/IMG_20220310_103203.jpg)",
+  //   "url(https://e5-oneindex.vercel.app/api/raw/?path=/picbed/IMG_20220310_103210.jpg)",
+  //   "url(https://e5-oneindex.vercel.app/api/raw/?path=/picbed/IMG_20220310_103220.jpg)",
+  //   "url(https://e5-oneindex.vercel.app/api/raw/?path=/picbed/IMG_20220310_103231.jpg)",
+  //   "url(https://e5-oneindex.vercel.app/api/raw/?path=/picbed/IMG_20220307_190940.jpg)"
+    
+    
+    
+  // ];
   
   //获取banner图片总数，生成随机数
   var bannerindex =Math.floor(Math.random() * bannerimg.length);
